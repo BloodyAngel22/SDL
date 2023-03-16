65b213c920ec729361e4adfee7a82f5de4375853
BloodyAngel22 <NanoViper222@yandex.ru>
2023-03-16 17:55:59 +0300

    upd Trapeze with lines, Circle, Filled Circle, Circle through rectangles

--- SDL_test/Source.cpp
+++ SDL_test/Source.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <math.h>
 
 int win_width = 1280, win_height = 720;
 SDL_Window* win = NULL;
@@ -48,7 +49,7 @@ int main(int argc, char* argv[]) {
 	SDL_RenderClear(ren);
 	
 
-	//Флаг
+	//Flag
 	/*SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
 	for (int i = 0; i < 100; i++) {
 		SDL_RenderDrawLine(ren, 400, win_height / 2 - 100 - i, win_width - 400, win_height / 2 - 100 - i);
@@ -65,25 +66,63 @@ int main(int argc, char* argv[]) {
 		SDL_RenderPresent(ren);
 	}*/
 
-	//ось 
+	//axis 
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
-	for (int i = 0; i < 5; i++) {
-		SDL_RenderDrawLine(ren, 0, win_height / 2 + 5 - i, win_width, win_height / 2 + 5 - i);
-		SDL_RenderDrawLine(ren, win_width / 2 - 5 + i, 0, win_width / 2 - 5 + i, win_height);
-	}
+	SDL_RenderDrawLine(ren, 0, win_height / 2, win_width, win_height / 2);
+	SDL_RenderDrawLine(ren, win_width / 2, 0, win_width / 2, win_height);
 
-	//парабола
-	for (float x = 0; x < 50; x = x + 0.005) {
+	//parabola
+	/*for (float x = 0; x < 50; x = x + 0.005) {
 		float y = x * x;
 		SDL_RenderDrawPoint(ren, win_width / 2 + x + 1, win_height / 2 - y + 2);
 		SDL_RenderDrawPoint(ren, win_width / 2 - x - 5, win_height / 2 - y + 2);
 		SDL_RenderPresent(ren);
-	}
+	}*/
 
+	//Trapeze with lines
+	SDL_SetRenderDrawColor(ren, 255, 0, 0, 0);
+	/*for (float i = 0; i < 200; i+=0.005) {
+		SDL_RenderDrawLine(ren, win_width / 2, win_height / 2 - i, win_width / 2 + 300 - i, win_height / 2 - i);
+	}
+	for (float i = 0; i < 200; i += 0.005) {
+		SDL_RenderDrawLine(ren, win_width / 2 -300 + i, win_height / 2 - i, win_width / 2, win_height / 2 - i);
+	}*/
+
+	//Circle
+	/*int r = 250, x, y;
+	for (float alpha = 0; alpha <= 360; alpha += 0.1) {
+		x = (r * cos(alpha)) + win_width / 2;
+		y = (r * sin(alpha)) + win_height / 2;
+		SDL_RenderDrawPoint(ren, x, y);
+	}*/
+
+	//Filled Circle
+	/*int r = 100, x, y;
+	while (r >= 0) {
+		for (float alpha = 0; alpha <= 360; alpha += 0.1) {
+			x = (r * cos(alpha)) + win_width / 2;
+			y = (r * sin(alpha)) + win_height / 2;
+			SDL_RenderDrawPoint(ren, x, y);
+		}
+		r--;
+	}*/
+
+	//Circle through rectangles
+	int r = 300, x, y;
+	SDL_Rect rect = { 0, 0, 5,5 };
+	while (r >= 0) {
+		for (float alpha = 0; alpha <= 360; alpha+= 0.6) {
+			x = r * cos(alpha);
+			y = r * sin(alpha);
+			rect = { x + win_width / 2, y + win_height / 2, 5, 5 };
+			SDL_RenderFillRect(ren, &rect);
+		}
+		r--;
+	}
 
 	SDL_RenderPresent(ren);
 
-	SDL_Delay(3000);
+	SDL_Delay(5000);
 
 	de_init(0);
 	return 0;
